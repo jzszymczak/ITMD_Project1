536081971f7d06b10f1231cf5ab16a693786c5e0
jzszymczak <jszymczak@hawk.iit.edu>
2025-04-15 18:36:04 -0500

    added three extra features to the map

--- js/script.js
+++ js/script.js
@@ -24,28 +24,42 @@ function initMap() {
   const location = { lat: 41.83673948259765, lng: -87.62601463238548 };
   const climbGym = { lat: 41.945534641482034, lng:-87.71038545605438};
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
+    zoom: 10,
     center: location,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-      position: google.maps.ControlPosition.TOP_LEFT,
-      mapTypeIds: [
-        google.maps.MapTypeId.ROADMAP,
-        google.maps.MapTypeId.SATELLITE,
-      ],
-    },  
+  });
+  
+  const infoWindow1 = new google.maps.InfoWindow({
+    content: "<h3>Illinois Tech</h3><p>This is where I go to school</p>",
+  });
+  const infoWindow2 = new google.maps.InfoWindow({
+    content: "<h3>First Ascent</h3><p>This is the climbing gym I go to</p>",
   });
 
-  new google.maps.Marker({
+  const schoolMarker = new google.maps.Marker({
     position: location, 
     map: map,
-    title: "IIT Campus"
+    title: "IIT Campus",
+    icon:  {
+      url: "extras/building.svg",
+      scaledSize: new google.maps.Size(32,32)
+    }
   });
 
-  new google.maps.Marker({
+  const gymMarker = new google.maps.Marker({
     position: climbGym,
     map: map,
-    title: "First Ascent"
+    title: "First Ascent",
+    icon: {
+      url: "extras/fitness-centre.svg",
+      scaledSize: new google.maps.Size(32,32)
+    }
   });
+
+  schoolMarker.addListener("click", () => {
+    infoWindow1.open(map, schoolMarker);
+  });
+
+  gymMarker.addListener("click", () => {
+    infoWindow2.open(map, gymMarker);
+  })
 }
